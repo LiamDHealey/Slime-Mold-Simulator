3b926dea23d7d3f1dd50f61b89f465e82a027c8e
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-04-02 14:54:21 -0600

    Update ComputeController.cs

--- Assets/Simulation/ComputeController.cs
+++ Assets/Simulation/ComputeController.cs
@@ -13,59 +13,35 @@ public class ComputeController : MonoBehaviour
     [TitleGroup("Agents/Spawning")]
     public int numAgents = 100;
 
-
     [TitleGroup("Agents/Spawning")]
     [Range(0,1)]
     public float normalizedSpawnRadius = 0.25f;
-
 
     [TitleGroup("Agents/Movement")]
     public float agentSpeed = 1.0f;
 
 
     [FoldoutGroup("References", false)]
-    public ComputeShader shader;
+    public ComputeShader agentShader;
 
+    [FoldoutGroup("References")]
+    public ComputeShader diffusionShader;
 
     [FoldoutGroup("References")]
     public RenderTexture texture;
 
-    private ComputeBuffer buffer;
-    private int kernelHandle;
+
+
+
+    private ComputeBuffer agentBuffer;
+    private RenderTexture diffusionTexture;
+    private int agentKernel;
+    private int diffusionKernel;
 
     private void Awake()
     {
         GetComponent<RawImage>().texture = texture;
-    }
-
-    [GUIColor(0.5f,1f,0.5f)]
-    [Button]
-    public void ResetSimulation()
-    {
-        kernelHandle = shader.FindKernel("CSMain");
-
-        texture.Release();
-        texture.Create();
-        shader.SetTexture(kernelHandle, "Result", texture);
-        shader.SetFloats("MaxBounds", new float[] { texture.width, texture.height, 0, 0 });
-
-
-        Agent[] data = new Agent[numAgents];
-        for (int i = 0; i < numAgents; i++)
-        {
-            Vector2 position = Random.insideUnitCircle * normalizedSpawnRadius * texture.height / 2f + new Vector2(texture.width / 2f, texture.height / 2f);
-            Vector2 forward = Random.insideUnitCircle.normalized;
-            data[i] = new Agent(position, forward);
-        }
-        
-        buffer?.Release();
-        buffer = new ComputeBuffer(data.Length, Agent.sizeOf);
-        buffer.SetData(data);
-        shader.SetBuffer(kernelHandle, "Agents", buffer);
-
-        shader.Dispatch(kernelHandle, buffer.count / 32, 1, 1);
-
-        Debug.Log("Dispatched!");
+        diffusionTexture = new(texture);
     }
 
     private void Start()
@@ -78,9 +54,41 @@ public class ComputeController : MonoBehaviour
         if (!Application.isPlaying)
             return;
 
-        shader.SetFloat("AgentSpeed", agentSpeed * Time.deltaTime);
-        shader.SetFloat("Time", agentSpeed * Time.time);
-        shader.Dispatch(kernelHandle, buffer.count / 32, 1, 1);
+
+
+        agentShader.SetFloat("AgentSpeed", agentSpeed * Time.deltaTime);
+        agentShader.SetFloat("Time", agentSpeed * Time.time);
+        agentShader.Dispatch(agentKernel, agentBuffer.count / 32, 1, 1);
+    }
+
+    [GUIColor(0.5f, 1f, 0.5f)]
+    [Button]
+    public void ResetSimulation()
+    {
+        kernelHandle = shader.FindKernel("CSMain");
+
+        texture.Release();
+        texture.Create();
+        agentShader.SetTexture(agentKernel, "Result", texture);
+        agentShader.SetFloats("MaxBounds", new float[] { texture.width, texture.height, 0, 0 });
+
+
+        Agent[] data = new Agent[numAgents];
+        for (int i = 0; i < numAgents; i++)
+        {
+            Vector2 position = Random.insideUnitCircle * normalizedSpawnRadius * texture.height / 2f + new Vector2(texture.width / 2f, texture.height / 2f);
+            Vector2 forward = Random.insideUnitCircle.normalized;
+            data[i] = new Agent(position, forward);
+        }
+
+        agentBuffer?.Release();
+        agentBuffer = new ComputeBuffer(data.Length, Agent.sizeOf);
+        agentBuffer.SetData(data);
+        agentShader.SetBuffer(agentKernel, "Agents", agentBuffer);
+
+        agentShader.Dispatch(agentKernel, agentBuffer.count / 32, 1, 1);
+
+        Debug.Log("Dispatched!");
     }
 
 }
