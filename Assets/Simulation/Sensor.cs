50c76858d7de8dffefb40e21a83f3b245e510adc
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-04-04 11:40:24 -0600

    Update shaders

--- Assets/Simulation/Sensor.cs
+++ Assets/Simulation/Sensor.cs
@@ -3,14 +3,16 @@ using System;
 [Serializable]
 public struct Sensor
 {
-    public const int sizeOf = 8;
+    public const int sizeOf = 12;
 
     public float angle;
     public float distance;
+    public float turnSpeed;
 
-    public Sensor(float angle, float distance)
+    public Sensor(float angle, float distance, float turnSpeed)
     {
         this.angle = angle;
         this.distance = distance;
+        this.turnSpeed = turnSpeed;
     }
 }
