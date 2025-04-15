7f104a865f4c3c9d637959f19146bd7e467c7531
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-04-15 09:16:14 -0600

    Add camera controls

---
+++ Assets/Rendering/CameraMover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float zoomSpeed = 1;
+    public float rotationSpeed = 1;
+
+    float distance;
+    Vector3 lastMousePos;
+
+    private void Start()
+    {
+        lastMousePos = Input.mousePosition;
+        distance = transform.localPosition.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance += Input.mouseScrollDelta.y * -zoomSpeed;
+
+        if (Input.GetMouseButton(0))
+        {
+            var delta = Input.mousePosition - lastMousePos;
+            delta = new(-delta.y, delta.x, delta.z);
+            transform.localRotation *= Quaternion.Euler(delta * rotationSpeed);
+        }
+
+        transform.localPosition = transform.forward * -distance;
+
+
+        lastMousePos = Input.mousePosition;
+    }
+}
