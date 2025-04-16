84e3bc8b0b0defb4cfa705ea94216c159b2127b2
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2025-04-16 17:01:50 -0600

    Better camera

--- Assets/Rendering/CameraMover.cs
+++ Assets/Rendering/CameraMover.cs
@@ -6,51 +6,56 @@ using UnityEngine.UIElements;
 
 public class CameraMover : MonoBehaviour
 {
-    public float zoomSpeed = 1;
+    public float zoomSpeed = 1f;
+    public float moveSpeed = 1;
     public float rotationSpeed = 1;
 
     float distance;
-    private float right;
     Vector3 lastMousePos;
 
     private void Start()
     {
         lastMousePos = Input.mousePosition;
-        distance = transform.localPosition.magnitude;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //distance = Mathf.Clamp(distance + Input.mouseScrollDelta.y * -zoomSpeed, -5, 4f);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            distance = Mathf.Clamp(distance - zoomSpeed, -5, 4f);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            distance = Mathf.Clamp(distance + zoomSpeed, -5, 4f);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            right = Mathf.Clamp(right - zoomSpeed, -5, 5f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            right = Mathf.Clamp(right + zoomSpeed, -5, 5f);
-        }
-
-        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        if ((Input.GetMouseButton(0) || Input.GetMouseButton(1)) && !EventSystem.current.IsPointerOverGameObject())
         {
             var delta = Input.mousePosition - lastMousePos;
             delta = new(-delta.y, delta.x, delta.z);
             transform.localRotation *= Quaternion.Euler(delta * rotationSpeed);
         }
-
-        transform.localPosition = transform.forward * -distance + transform.right * right;
-
-
         lastMousePos = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))
+        {
+            distance = transform.localPosition.magnitude;
+            transform.rotation = Quaternion.LookRotation(-transform.localPosition.normalized, transform.up);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            distance = Mathf.Clamp(distance + Input.mouseScrollDelta.y * -zoomSpeed, -5, 4f);
+            transform.localPosition = -transform.forward * distance;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.localPosition += transform.forward * moveSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.localPosition += -transform.forward * moveSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.localPosition += -transform.right * moveSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.localPosition += transform.right * moveSpeed * Time.deltaTime;
+            }
+        }
     }
 }
